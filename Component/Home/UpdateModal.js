0cee37bd377a3339622d83311710b99fac413dca
Wade Butler <wadebutler@MacBook-Pro.local>
2025-01-13 15:18:59 -0500

    modal update

--- Component/Home/UpdateModal.js
+++ Component/Home/UpdateModal.js
@@ -3,37 +3,40 @@ import FFText from "../Global/FFText";
 import { A } from "@expo/html-elements";
 
 export default function UpdateModal({ expand, handleExpand }) {
-    return (
-        <View
-            style={[
-                styles.modalContainer,
-                !expand ? { height: 0 } : { height: "100%" },
-            ]}
-        >
-            <View style={styles.container}>
-                <FFText>spring points update</FFText>
+  return (
+    <View
+      style={[
+        styles.modalContainer,
+        !expand ? { height: 0 } : { height: "100%" },
+      ]}
+    >
+      <View style={styles.container}>
+        <FFText>
+          the data and points are out of date please use this app at your own
+          caution.
+        </FFText>
 
-                <A style={styles.linkText} href="https://discord.gg/v3kszadk">
+        {/* <A style={styles.linkText} href="https://discord.gg/v3kszadk">
                     Join the Discord
-                </A>
-            </View>
-        </View>
-    );
+                </A> */}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 40,
-        paddingHorizontal: 10,
-    },
-    linkText: {
-        marginTop: 10,
-        color: "blue",
-        fontSize: 20,
-    },
-    closeButton: {
-        position: "absolute",
-        right: 10,
-        top: 40,
-    },
+  container: {
+    marginTop: 40,
+    paddingHorizontal: 10,
+  },
+  linkText: {
+    marginTop: 10,
+    color: "blue",
+    fontSize: 20,
+  },
+  closeButton: {
+    position: "absolute",
+    right: 10,
+    top: 40,
+  },
 });
